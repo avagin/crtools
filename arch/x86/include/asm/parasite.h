d16a8c6c85490c5be7bcb74a480e4357bc5052ce
Cyrill Gorcunov <gorcunov@openvz.org>
2013-02-26 13:29:51 +0400

    x86: parasite -- Add call_daemon_thread and asm_trap helpers
    call_daemon_thread will be needed to run thread daemon
    with own stack, while asm_trap is just a wrap over trapping
    instruction for portability sake.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- arch/x86/include/asm/parasite.h
+++ arch/x86/include/asm/parasite.h
@@ -6,4 +6,30 @@ static inline u32 arch_get_tls()
 	return 0;
 }
 
+/*
+ * Call thread daemon with new stack, the function must
+ * return the original stack pointer passed in argument
+ * argument 3
+ */
+#define call_daemon_thread(new_sp, args, s, func)	\
+do {							\
+	asm volatile ("movq %0, %%rax		\n"	\
+		      "movq %1, %%rdi		\n"	\
+		      "movq %2, %%rsi		\n"	\
+		      "movq %%rsp, %%rdx	\n"	\
+		      "movq %%rax, %%rsp	\n"	\
+		      "call " #func "		\n"	\
+		      "movq %%rax, %%rsp	\n"	\
+		      :					\
+		      : "g"(new_sp), "g"(args), "g"(s)	\
+		      : "rax", "rdi", "rsi", "rdx",	\
+		        "rsp", "memory");		\
+} while (0)
+
+#define asm_trap()					\
+do {							\
+	asm volatile("int3" ::);			\
+	while (1) ;					\
+} while (0)
+
 #endif
