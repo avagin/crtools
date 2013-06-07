8e6d4fc2f1e342049c1baca10e9d30a57a787bf8
Andrey Vagin <avagin@openvz.org>
2013-06-08 00:22:27 +0400

    dump: move code for dumping threads in a separate function
    The next patch will add another version of this function,
    which will use PTRACE_SETSIGMASK.

--- parasite-syscall.c
+++ parasite-syscall.c
@@ -472,6 +472,50 @@ err:
 	return -1;
 }
 
+static int dump_thread_in_two_stage(struct parasite_ctl *ctl, int id,
+				struct pid *tid, CoreEntry *core)
+{
+	struct parasite_dump_thread *args;
+	user_regs_struct_t regs_orig;
+	pid_t pid = tid->real;
+	int ret;
+
+	args = parasite_args(ctl, struct parasite_dump_thread);
+
+	ret = ptrace(PTRACE_GETREGS, pid, NULL, &regs_orig);
+	if (ret) {
+		pr_perror("Can't obtain registers (pid: %d)", pid);
+		return -1;
+	}
+
+	ret = parasite_execute_trap_by_pid(PARASITE_CMD_INIT_THREAD, ctl,
+					pid, &regs_orig,
+					ctl->r_thread_stack, false);
+	if (ret) {
+		pr_err("Can't init thread in parasite %d\n", pid);
+		return -1;
+	}
+
+	ret = get_task_regs(pid, regs_orig, core);
+	if (ret)
+		pr_err("Can't obtain regs for thread %d\n", pid);
+
+	if (parasite_execute_trap_by_pid(PARASITE_CMD_FINI_THREAD, ctl,
+					pid, &regs_orig,
+					ctl->r_thread_stack, true)) {
+		pr_err("FATAL: Can't fini thread in parasite %d\n", pid);
+		return -1;
+	}
+	if (ret)
+		return -1;
+
+	memcpy(&core->thread_core->blk_sigset,
+		&args->blocked, sizeof(k_rtsigset_t));
+
+	core->thread_core->has_blk_sigset = true;
+	return 0;
+}
+
 int parasite_dump_thread_seized(struct parasite_ctl *ctl, int id,
 				struct pid *tid, CoreEntry *core)
 {
@@ -482,41 +526,8 @@ int parasite_dump_thread_seized(struct parasite_ctl *ctl, int id,
 
 	if (id == 0)
 		ret = parasite_execute_daemon(PARASITE_CMD_DUMP_THREAD, ctl);
-	else {
-		pid_t pid = tid->real;
-		user_regs_struct_t regs_orig;
-
-		ret = ptrace(PTRACE_GETREGS, pid, NULL, &regs_orig);
-		if (ret) {
-			pr_perror("Can't obtain registers (pid: %d)", pid);
-			return -1;
-		}
-
-		ret = parasite_execute_trap_by_pid(PARASITE_CMD_INIT_THREAD, ctl,
-						pid, &regs_orig,
-						ctl->r_thread_stack, false);
-		if (ret) {
-			pr_err("Can't init thread in parasite %d\n", pid);
-			return -1;
-		}
-
-		ret = get_task_regs(pid, regs_orig, core);
-		if (ret)
-			pr_err("Can't obtain regs for thread %d\n", pid);
-
-		if (parasite_execute_trap_by_pid(PARASITE_CMD_FINI_THREAD, ctl,
-						pid, &regs_orig,
-						ctl->r_thread_stack, true)) {
-			pr_err("Can't init thread in parasite %d\n", pid);
-			return -1;
-		}
-		if (ret)
-			return -1;
-
-		memcpy(&core->thread_core->blk_sigset,
-			&args->blocked, sizeof(k_rtsigset_t));
-		core->thread_core->has_blk_sigset = true;
-	}
+	else
+		ret = dump_thread_in_two_stage(ctl, id, tid, core);
 
 	CORE_THREAD_ARCH_INFO(core)->clear_tid_addr = encode_pointer(args->tid_addr);
 	tid->virt = args->tid;
