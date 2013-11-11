f30924697c9bd04d99747bd026e966d3dc228822
Andrey Vagin <avagin@openvz.org>
2013-11-11 13:27:32 +0400

    mounts: allow to customize root path for cr_pivot_root
    We are going to restore nested mount namespaces and we will need to
    change root for each of them.

--- mount.c
+++ mount.c
@@ -1262,11 +1262,16 @@ static int clean_mnt_ns(void)
 	return mnt_tree_for_each_reverse(mntinfo_tree, do_umount_one);
 }
 
-static int cr_pivot_root(void)
+static int cr_pivot_root(char *root)
 {
 	char put_root[] = "crtools-put-root.XXXXXX";
 
-	pr_info("Move the root to %s\n", opts.root);
+	pr_info("Move the root to %s\n", root);
+
+	if (chdir(root)) {
+		pr_perror("chdir(%s) failed", root);
+		return -1;
+	}
 
 	if (mkdtemp(put_root) == NULL) {
 		pr_perror("Can't create a temporary directory");
@@ -1277,6 +1282,11 @@ static int cr_pivot_root(void)
 		pr_perror("pivot_root(., %s) failed", put_root);
 		if (rmdir(put_root))
 			pr_perror("Can't remove the directory %s", put_root);
+		return -1;
+	}
+
+	if (mount("none", put_root, "none", MS_REC|MS_PRIVATE, NULL)) {
+		pr_perror("Can't remount root with MS_PRIVATE");
 		return -1;
 	}
 
@@ -1504,7 +1514,7 @@ int prepare_mnt_ns(int ns_pid)
 		goto out;
 
 	if (opts.root)
-		ret = cr_pivot_root();
+		ret = cr_pivot_root(opts.root);
 out:
 	return ret;
 }
